8230103e79b0081852138a81f9af6b185e566abd
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2024-02-25 09:06:59 -0300

    feat: create bcrypt to hash passwords

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
+import { hash } from 'bcryptjs'
 import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
 
@@ -11,8 +12,24 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    reply.status(409).send()
+  }
+
   await prisma.user.create({
-    data: { name, email, password_hash: password },
+    data: {
+      name,
+      email,
+      password_hash,
+    },
   })
 
   return reply.status(201).send()
