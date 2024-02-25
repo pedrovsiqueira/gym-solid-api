78caf3d8d3febb386e1855ec9e530c89f30a8336
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2024-02-25 13:44:57 -0300

    feat: create errors logic

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
 import { RegisterService } from '../../services/register'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/services/errors/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -22,7 +23,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       password,
     })
   } catch (error) {
-    reply.status(409).send()
+    if (error instanceof UserAlreadyExistsError) {
+      reply.status(409).send({ message: error.message })
+    }
+
+    return reply.status(500).send() // TODO: update this in the future
   }
 
   return reply.status(201).send()
