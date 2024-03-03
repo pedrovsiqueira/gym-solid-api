b8eb7f87fbde0c8263d3deecf67c8081c2bab895
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2024-03-03 17:08:09 -0300

    feat: create logic to handle validate check in

---
+++ src/services/validate-check-in.spec.ts
@@ -0,0 +1,42 @@
+import { afterEach, beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { ValidateCheckInService } from './validate-check-in'
+import { ResourceNotFoundError } from './errors/resource-not-found'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let validateCheckInService: ValidateCheckInService
+
+describe('Validate Check In Use Case', () => {
+  beforeEach(async () => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    validateCheckInService = new ValidateCheckInService(checkInsRepository)
+
+    // vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    // vi.useRealTimers()
+  })
+
+  it('should be able to validate check-in', async () => {
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    const { checkIn } = await validateCheckInService.execute({
+      checkInId: createdCheckIn.id,
+    })
+
+    expect(checkIn.validated_at).toEqual(expect.any(Date))
+    expect(checkInsRepository.items[0].validated_at).toEqual(expect.any(Date))
+  })
+
+  it('should not be able to validate an inexistent check-in', async () => {
+    await expect(() =>
+      validateCheckInService.execute({
+        checkInId: 'inexistent-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
